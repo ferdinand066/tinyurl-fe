83ba6cecb98b416a1caf6ccd8e37b6f5ecf26e44
Ferdinand Gunawan <ferdinandg066@gmail.com>
2023-02-09 18:04:33 +0700

    fix: fix button layout

--- src/components/_url-result.tsx
+++ src/components/_url-result.tsx
@@ -4,19 +4,25 @@ import { TinyUrl } from "@/handlers/types/tiny-url";
 import { ActionHandlers } from "@/handlers/utilities/action-handlers";
 
 export default function UrlResult({ generatedUrl, text }: { generatedUrl: TinyUrl | RenameUrl | undefined, text: string }) {
-  return generatedUrl ? <div className='sm:max-w-xl sm:mx-auto lg:mx-0'>
-    <div className='flex flex-col w-full gap-2 px-4 py-3 text-base text-gray-900 break-all bg-white border-0 rounded-md'>
-      <span>{generatedUrl.url}</span>
-      <span className='text-sm text-center'>{ text }</span>
-      <div className='flex flex-row items-center justify-between'>
-        <span className='flex-1 text-left'>{generatedUrl.label}</span>
-        <PrimaryButton 
-          content="Copy" 
-          type="button" 
-          className="w-20 py-1.5 text-center" 
-          onClick={() => ActionHandlers.copyText(generatedUrl.label)} 
-        />
+  return generatedUrl ? (
+    <div className="sm:max-w-xl sm:mx-auto lg:mx-0">
+      <div className="flex flex-col w-full gap-2 px-4 py-3 text-base text-gray-900 break-all bg-white border-0 rounded-md">
+        <span>{generatedUrl.url}</span>
+        <span className="text-sm text-center">{text}</span>
+        <div className="flex flex-row items-center justify-between">
+          <span className="flex-1 text-left">{generatedUrl.label}</span>
+          <div className="w-20">
+            <PrimaryButton
+              content="Copy"
+              type="button"
+              className="!py-1.5 !text-center"
+              onClick={() => ActionHandlers.copyText(generatedUrl.label)}
+            />
+          </div>
+        </div>
       </div>
     </div>
-  </div> : <></>
+  ) : (
+    <></>
+  )
 }
